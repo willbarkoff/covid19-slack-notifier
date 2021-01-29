40bb4fcb8370b7d0169a162920bae444464ba624
William Barkoff <william@barkoffusa.com>
2021-01-28 19:29:17 -0500

    intiial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import DotEnv from 'dotenv';
+import { App, LogLevel } from '@slack/bolt';
+import fetch from 'node-fetch';
+
+DotEnv.config();
+
+const channelID = "C01L9DB4JKD"
+
+// 30 second interval
+const interval = 30 * 1000
+
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+	token: process.env.SLACK_BOT_TOKEN,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
+	logLevel: LogLevel.DEBUG,
+});
+
+(async () => {
+	await app.start(process.env.PORT || 3000);
+
+	setInterval(async () => {
+		let fetchresp = await fetch("https://nycvaccinelist.com/api/locations");
+		let resp = await fetchresp.json()
+
+		let availableLocations = resp.locations.filter(location => location.total_available > 0);
+
+		if (availableLocations.length < 1) {
+			return;
+		}
+
+		try {
+			const result = await app.client.chat.postMessage({
+				channel: channelID,
+				text: `There are vaccines available at ${availableLocations.map(loc => loc.name).join()}. Check nycvaccinelist.com for more information.`,
+				token: process.env.SLACK_BOT_TOKEN,
+			})
+			console.log(result)
+		} catch (e) {
+			console.error(e)
+		}
+
+	}, interval);
+})();
